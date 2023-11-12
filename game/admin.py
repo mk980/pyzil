80c0493f96cf2cc74bdeafd0b721905e59b56db9
Vasilis0108 <billnikolouzos@gmail.com>
2023-11-12 19:27:05 +0100

    Update Admin Site

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Player, Game, Question, AnswersSet
 # Register your models here.
+
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(Question)
+admin.site.register(AnswersSet)
