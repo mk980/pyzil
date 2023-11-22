863988529bac2c1cc39439dc72913b40b8aa4939
Artjom <artjomprus@gmail.com>
2023-11-22 17:14:01 +0200

    Fixing the encoding

--- game/views.py
+++ game/views.py
@@ -1,106 +1,85 @@
+import logging
 from django.shortcuts import redirect, render
 from .utils import fetch_question
 from django.contrib import messages
 from .models import AnswersSet, TriviaQuestion
 
+# Configure logging settings
+logging.basicConfig(level=logging.DEBUG)
 
 def index(request):
-	return render(request, 'game/index.html')
-
+    return render(request, 'game/index.html')
 
 def game_view(request):
-	if request.method == 'POST':
-		# Handle answer submission
-		question = request.POST.get('question')
-		user_answer = request.POST.get('option')
-		correct_answer = request.POST.get('answer_label')
+    logging.debug("Entered game_view function")
 
-		if user_answer == correct_answer:
-			messages.success(request, 'Correct answer')
-			return handle_next_question(request)
-		attempts = request.session.get('attempts', 0)
-		request.session['attempts'] = attempts + 1
+    if request.method == 'POST':
+        logging.debug("Received POST request")
 
-		if attempts >= 2:  # User has no further attempts left
-			messages.warning(request, f'Wrong answer, Correct Answer is {correct_answer}')
-			return handle_next_question(request)
+        # Handle answer submission
+        question = request.POST.get('question')
+        user_answer = request.POST.get('option')
+        correct_answer = request.POST.get('answer_label')
 
-		messages.warning(request, f'Wrong answer, try again. Attempts remaining: {2 - attempts}')
+        if user_answer == correct_answer:
+            messages.success(request, 'Correct answer')
+            return handle_next_question(request)
+        else:
+            attempts = request.session.get('attempts', 0)
+            request.session['attempts'] = attempts + 1
 
-		next_question_id = get_next_question(request)
+            if attempts >= 2:  # User has no further attempts left
+                messages.warning(request, f'Wrong answer, Correct Answer is {correct_answer}')
+                return handle_next_question(request)
 
-	response = fetch_question()
-	if response.status_code == 200:
-		data = response.json()
-		results = data.get("results", {"results": "No results"})
+            messages.warning(request, f'Wrong answer, try again. Attempts remaining: {2 - attempts}')
 
-		if results:
-			for result in results:
-				incorrect_answers = result.get("incorrect_answers")
-				# AnswerSet instance for Associated TriviaQuestion
-				answers_set = AnswersSet(
-					correct_answer=result.get("correct_answer"),
-					incorrect_answer1=incorrect_answers[0],
-					incorrect_answer2=incorrect_answers[1],
-					incorrect_answer3=incorrect_answers[2]
-				)
-				answers_set.save()
-				# TriviaQuestion instance
-				question = TriviaQuestion(
-					category=result.get("category"),
-					difficulty=result.get("difficulty"),
-					question=result.get("question"),
-					type=result.get("type"),
-					answer_set=answers_set
+    # Fetch a new question if no answer was submitted or the answer was incorrect
+    response = fetch_question()
+    if response.status_code == 200:
+        data = response.json()
+        results = data.get("results", [])
 
-				)
-				question.save()
-	question = TriviaQuestion.objects.order_by('?').first()
-	return render(request, 'game/game_screen.html', {'question': question})
+        if results:
+            for result in results:
+                # Save the question and its answers
+                incorrect_answers = result.get("incorrect_answers")
+                answers_set = AnswersSet(
+                    correct_answer=result.get("correct_answer"),
+                    incorrect_answer1=incorrect_answers[0],
+                    incorrect_answer2=incorrect_answers[1],
+                    incorrect_answer3=incorrect_answers[2]
+                )
+                answers_set.save()
 
+                question = TriviaQuestion(
+                    category=result.get("category"),
+                    difficulty=result.get("difficulty"),
+                    question=result.get("question"),
+                    type=result.get("type"),
+                    answer_set=answers_set
+                )
+                question.save()
+
+    # Display the next question
+    question_counter = request.session.get('question_counter', 0)
+    attempts = request.session.get('attempts', 0)
+    current_question_id = request.session.get('current_question')
+
+    logging.debug(f"Question counter: {question_counter}")
+    logging.debug(f"Attempts: {attempts}")
+    logging.debug(f"Current question ID: {current_question_id}")
+
+    question = TriviaQuestion.objects.order_by('?').first()
+    return render(request, 'game/game_screen.html', {'question': question})
 
 def handle_next_question(request):
-	# Increment the session's question counter
-	question_counter = request.session.get('question_counter', 0)
-	request.session['question_counter'] = question_counter + 1
+    question_counter = request.session.get('question_counter', 0)
+    request.session['question_counter'] = question_counter + 1
 
-	if question_counter >= 10:  # Max questions for the game
-		# Game finished, redirect to a completion page or take other actions
-		return render(request, 'game/game_finished.html')
+    request.session['attempts'] = 0  # Reset attempts for the new question
 
-	# Try to get the next question
-	current_question_id = request.session.get('current_question')
+    if question_counter >= 9:  # Max questions for the game (starting from 0)
+        return render(request, 'game/game_finished.html')
 
-	if current_question_id is not None:
-		try:
-			next_question = TriviaQuestion.objects.get(pk=current_question_id)
-		except TriviaQuestion.DoesNotExist:
-			next_question = None
-	else:
-		next_question = None
-
-	if next_question is None:
-		# No more questions available, handle it (e.g., fetch a new question or redirect)
-		return render(request, 'game/no_more_questions.html')
-
-	# Append the ID of the next question to the asked_questions session variable
-	request.session.setdefault('asked_questions', []).append(next_question.pk)
-	request.session['attempts'] = 0  # Reset attempts for the new question
-	request.session['current_question'] = next_question.pk
-
-	return redirect('game_view')
-
-
-def get_next_question(request):
-	# Try to get the next question
-	asked_question_ids = request.session.get('asked_questions', [])
-	print("asked_question_ids:", asked_question_ids)
-	next_question = TriviaQuestion.objects.order_by('?').exclude(
-		pk__in=asked_question_ids
-	).first()
-	if next_question:
-		next_question_id = next_question.pk
-	else:
-		next_question_id = None
-
-	return next_question_id
+    return redirect('game_view')
