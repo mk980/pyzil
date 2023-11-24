b7afc10c92d4777b77fbd3d580d1e5771b32159f
mk980 <malickfiaz@aol.com>
2023-11-24 03:14:43 +0200

    Bootstrap added for visualization. Answer randomizer is added from feature/random

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name='index'),
-	path("game_screen/", views.game_view, name='game_view')
+	path("game_screen/", views.game_view, name='game_view'),
+	path("incorrect_answer/", views.game_view, name='game_view')
 ]
