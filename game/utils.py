0fe0fdb78b632d728b0d31232c8d64bccd9a92c8
Artjom <artjomprus@gmail.com>
2023-11-25 20:06:09 +0200

    new logic

--- game/utils.py
+++ game/utils.py
@@ -1,12 +1,10 @@
-from django.http import JsonResponse
 import requests
-from .models import GameSession, AnswersSet, TriviaQuestion
 
 
 def fetch_question():
     try:
         response = requests.get('https://opentdb.com/api.php?amount=10&type=multiple')
-        return response  # Return the response object directly
+        return response
     except Exception as e:
         print(f"Error fetching questions: {e}")
         return None
--- game/views.py
+++ game/views.py
@@ -1,7 +1,6 @@
 import logging
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from .utils import fetch_question
-from django.contrib import messages
 from .models import AnswersSet, TriviaQuestion, GameSession
 
 logging.basicConfig(level=logging.DEBUG)
@@ -44,8 +43,9 @@ def new_game_start(request):
         return render(request,
                       'game/game_screen.html',
                       {'question': question,
-                       "game": new_game,
+                       "game_id": new_game.pk,
                        'current_question_id': question.pk})
+
 
 def game_view(request):
     logging.debug("Entered game_view function")
@@ -56,12 +56,9 @@ def game_view(request):
     if request.method == "GET":
         return new_game_start(request)
 
-
-
     if request.method == 'POST':
         logging.debug("Received POST request")
         current_question_id = request.session.get('current_question')
-        question_id = request.POST.get('question')
         user_answer = request.POST.get('option')
         question = get_current_question(request)
         question.user_answer = user_answer
@@ -69,10 +66,20 @@ def game_view(request):
         question.save()
 
         next_question = get_next_question(request)
+        if next_question == None:
+            score = 0
+            game = GameSession.objects.get(pk=request.POST.get("game_id"))
+            for question in game.game_questions.all():
+                if question.user_answer == question.answer_set.correct_answer:
+                    score += 1
 
-        return render(request,
-               'game/game_screen.html',
-               {'question': next_question, 'current_question_id': current_question_id})
+            return render(request, "game/game_finished.html",
+                          {'score': score, "questioncount": game.game_questions.count()})
+        else:
+            return render(request,
+                          'game/game_screen.html',
+                          {'question': next_question, 'current_question_id': current_question_id,
+                           "game_id": request.POST.get("game_id")})
 
 
 def get_current_question(request):
@@ -82,10 +89,11 @@ def get_current_question(request):
 
 
 def get_next_question(request):
-    unanswered_questions = TriviaQuestion.objects.filter(is_answered=False)
+    current_game = GameSession.objects.get(pk=request.POST.get("game_id"))
+    unanswered_questions = current_game.game_questions.filter(is_answered=False)
     if unanswered_questions.count() > 0:
         return unanswered_questions.first()
-    else:
-        return render(request, "game/game_finished.html")
+
+
 
 
